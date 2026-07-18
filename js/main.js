a45297c66c518a3af1d602e6ca221bd621523049
hihiquan2010 <uyp079658@gmail.com>
2026-07-19 06:12:39 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -58,17 +58,17 @@ btn.addEventListener("click", function () {
     // BNDC
     case "Bình Ngô đại cáo":
       console.log("Người dùng đã chọn", "Bình Ngô đại cáo");
-      window.location.href = "/html/Bình Ngô đại cáo.html";
+      window.location.href = "./html/binh_ngo_dai_cao.html";
       break;
     // DTS
     case "Dục Thúy sơn":
       console.log("Người dùng đã chọn", "Dục Thúy sơn");
-      window.location.href = "/html/Dục Thúy sơn.html";
+      window.location.href = "./html/duc_thuy_son.html";
       break;
     // HTLNKCQG
     case "Hiền tài là nguyên khí của quốc gia":
       console.log("Người dùng đã chọn", "Hiền tài là nguyên khí của quốc gia");
-      window.location.href = "/html/Hiền tài là nguyên khí của quốc gia.html";
+      window.location.href = "./html/hien_tai_la_nguyen_khi_cua_quoc_gia.html";
       break;
     // NTTTCNV
     case "Nghệ thuật truyền thống của người Việt":
@@ -77,22 +77,22 @@ btn.addEventListener("click", function () {
         "Nghệ thuật truyền thống của người Việt",
       );
       window.location.href =
-        "/html/Nghệ thuật truyền thống của người Việt.html";
+        "./html/nghe_thuat_truyen_thong_cua_nguoi_Viet.html";
       break;
     // TVTPSL
     case "Tản Viên từ Phán sự lục":
       console.log("Người dùng đã chọn", "Tản Viên từ Phán sự lục");
-      window.location.href = "/html/Tản Viên từ Phán sự lục.html";
+      window.location.href = "./html/Tan_Vien_tu_Phan_su_luc.html";
       break;
     // TTT
     case "Thần Trụ Trời":
       console.log("Người dùng đã chọn", "Thần Trụ Trời");
-      window.location.href = "/html/Thần Trụ Trời.html";
+      window.location.href = "./html/Than_Tru_Troi.html";
       break;
     // XVGD
     case "Xúy Vân giả dại":
       console.log("Người dùng đã chọn", "Xúy Vân giả dại");
-      window.location.href = "/html/Xúy Vân giả dại.html";
+      window.location.href = "./html/Xuy_Van_gia_dai.html";
       break;
   }
 });
